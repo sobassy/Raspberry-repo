47479a144f5a6a787ef2f93fc4fe3f07a658cfca
Sobassy <altoscience1110@gmail.com>
2021-08-10 22:05:12 +0900

    add: express

--- node/expressServer/index.js
+++ node/expressServer/index.js
@@ -2,10 +2,28 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Welcome to Nogata Education API !!')
-})
+var gpio = require('rpi-gpio');
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+const LED_PIN = 40; // 40番ピン（GPIO40ではない、GPIO21）
+
+
+// LED ピンをOUT方向に設定
+gpio.setup(LED_PIN, gpio.DIR_OUT, () => {
+
+    // API start
+    app.get('/', (req, res) => {
+        res.send('Welcome to Nogata Education API !!')
+    })
+
+    app.get('/led/on', (req, res) => {
+        gpio.write(LED_PIN, true);
+    })
+
+    app.get('/led/off', (req, res) => {
+        gpio.write(LED_PIN, true);
+    })
+    
+    app.listen(port, () => {
+        console.log(`Example app listening at http://localhost:${port}`)
+    })
+});
