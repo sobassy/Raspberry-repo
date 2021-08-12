9df7fb81e2978f0a6725f614a737bba185bc5302
Sobassy <altoscience1110@gmail.com>
2021-08-12 09:56:26 +0900

    add: pwm source

--- Arduino/autocar/autocar.ino
+++ Arduino/autocar/autocar.ino
@@ -4,108 +4,95 @@
 #define left_PIN_2A 8
 #define right_PIN_1A 9
 #define right_PIN_2A 10
-//#define trigPin 11
-//#define echoPin 12
 
-//double duration = 0;
-//double distance = 0;
+int leftPower;
+int rightPower;
+
+String cmds[2];// 分割された文字列を格納する配列 
 
 void setup() {
   Serial.begin(9600);
-  pinMode(2, INPUT);
-  pinMode(3, INPUT);
-  pinMode(4, INPUT);
   pinMode(left_pwm, OUTPUT);
   pinMode(right_pwm, OUTPUT);
   pinMode(left_PIN_1A, OUTPUT);
   pinMode(left_PIN_2A, OUTPUT);
   pinMode(right_PIN_1A, OUTPUT);
   pinMode(right_PIN_2A, OUTPUT);
-  //pinMode(echoPin, INPUT);
-  //pinMode(trigPin, OUTPUT);
+
+  left_motor(HIGH, LOW);
+  right_motor(HIGH, LOW);
   
 }
 
 void loop() {
-// トレース
-  int pinValue1 = digitalRead(2);  //center
-  int pinValue2 = digitalRead(3);  //left
-  int pinValue3 = digitalRead(4);  //right
+//  Serial.print(leftPower);
+//  Serial.print(":");
+//  Serial.println(rightPower);or(HIGH, LOW);
 
-// 衝突回避
-//  digitalWrite(trigPin, LOW);
-//  delayMicroseconds(2);
-//  digitalWrite(trigPin, HIGH);
-//  delayMicroseconds(10);
-//  digitalWrite(trigPin, LOW);
+  analogWrite(left_pwm, leftPower);
+  analogWrite(right_pwm, rightPower);
+  
+  
+//  if (pinValue1 == 1) {
+//    analogWrite(left_pwm, 200);
+//    analogWrite(right_pwm, 200);
+//    left_motor(HIGH, LOW);
+//    right_motor(HIGH, LOW);
 //
-//  duration = pulseIn(echoPin, HIGH);
+//    if (pinValue2 == 1 && pinValue3 == 1){
+//      left_motor(LOW, LOW);
+//      right_motor(LOW, LOW);
+//      Serial.print("1");
+//      delay(500);
+//    }
 //
-//  duration = duration / 2;
-//  duration = duration / 1000000;
-//  distance = duration * 340;
-//  distance = distance * 100;
-  
-  if (pinValue1 == 1) {
-    analogWrite(left_pwm, 200);
-    analogWrite(right_pwm, 200);
-    left_motor(HIGH, LOW);
-    right_motor(HIGH, LOW);
-
-    if (pinValue2 == 1 && pinValue3 == 1){
-      left_motor(LOW, LOW);
-      right_motor(LOW, LOW);
-      Serial.print("1");
-      delay(500);
-    }
-
-    else if (pinValue2 == 1){
-      analogWrite(left_pwm, 100);
-      analogWrite(right_pwm, 255);
-      left_motor(HIGH, LOW);
-      right_motor(HIGH, LOW);
-      Serial.print("2");
-    }
-  
-    else if (pinValue3 == 1){
-      analogWrite(left_pwm, 255);
-      analogWrite(right_pwm, 100);
-      left_motor(HIGH, LOW);
-      right_motor(HIGH, LOW);
-      Serial.print("3");
-    }
-   }
-
-  else if (pinValue2 == 1) {
-    analogWrite(left_pwm, 100);
-    analogWrite(right_pwm, 255);
-    left_motor(HIGH, LOW);
-    right_motor(HIGH, LOW);
-    Serial.print("4");
-    
-
-   if (pinValue3 == 1) {
-    left_motor(LOW, LOW);
-    right_motor(LOW, LOW);
-    Serial.print("5");
-    delay(500);
-   }    
-  }
-
-  else if (pinValue3 == 1){
-    analogWrite(left_pwm, 255);
-    analogWrite(right_pwm, 100);
-    left_motor(HIGH, LOW);
-    right_motor(HIGH, LOW);
-    Serial.print("6");
-  }
-  
-  else{
-    left_motor(LOW, LOW);
-    right_motor(LOW, LOW);
-    Serial.print("7");
-    delay(500);
-  }
+//    else if (pinValue2 == 1){
+//      analogWrite(left_pwm, 100);
+//      analogWrite(right_pwm, 255);
+//      left_motor(HIGH, LOW);
+//      right_motor(HIGH, LOW);
+//      Serial.print("2");
+//    }
+//  
+//    else if (pinValue3 == 1){
+//      analogWrite(left_pwm, 255);
+//      analogWrite(right_pwm, 100);
+//      left_motor(HIGH, LOW);
+//      right_motor(HIGH, LOW);
+//      Serial.print("3");
+//    }
+//   }
+//
+//  else if (pinValue2 == 1) {
+//    analogWrite(left_pwm, 100);
+//    analogWrite(right_pwm, 255);
+//    left_motor(HIGH, LOW);
+//    right_motor(HIGH, LOW);
+//    Serial.print("4");
+//    
+//
+//   if (pinValue3 == 1) {
+//    left_motor(LOW, LOW);
+//    right_motor(LOW, LOW);
+//    Serial.print("5");
+//    delay(500);
+//   }    
+//  }
+//
+//  else if (pinValue3 == 1){
+//    analogWrite(left_pwm, 255);
+//    analogWrite(right_pwm, 100);
+//    left_motor(HIGH, LOW);
+//    right_motor(HIGH, LOW);
+//    Serial.print("6");
+//  }
+//  
+//  else{
+//    left_motor(LOW, LOW);
+//    right_motor(LOW, LOW);
+//    Serial.print("7");
+//    delay(500);
+//  }
 }
 
 void left_motor(int la1, int la2){
@@ -116,4 +103,31 @@ void left_motor(int la1, int la2){
 void right_motor(int ra1, int ra2){
   digitalWrite(right_PIN_1A, ra1);
   digitalWrite(right_PIN_2A, ra2);
+}
+
+
+void serialEvent() {
+  //データを1つ受信する関数（割り込み処理）です。
+  String myString = Serial.readStringUntil('\n');
+  int index = split(myString, ',', cmds);
+  Serial.println("data gets");
+     leftPower = cmds[0].toInt();
+     rightPower = cmds[1].toInt();
+}
+
+int split(String data, char delimiter, String *dst){
+     dst[0] = "";
+     dst[1] = "";
+    int index = 0;
+    int arraySize = (sizeof(data)/sizeof((data)[0]));  
+    int datalength = data.length();
+    for (int i = 0; i < datalength; i++) {
+        char tmp = data.charAt(i);
+        if ( tmp == delimiter ) {
+            index++;
+            if ( index > (arraySize - 1)) return -1;
+        }
+        else dst[index] += tmp;
+    }
+    return (index + 1);
 }
--- python/CameraTest.py
+++ python/CameraTest.py
@@ -1,13 +1,90 @@
 import cv2
+import numpy as np
+
+import serial
+
+ser = serial.Serial("/dev/cu.usbserial-14130", 9600, timeout=None) # シリアル通信の開始
+
+# vird view settings
+HEIGHT = 960
+WIDTH = 1280
+lane_shape = [(300, 600), (980, 600), (0, 960), (1280, 960)]
+top_left, top_right, bottom_left, bottom_right = lane_shape
+source = np.float32([top_left, top_right, bottom_right, bottom_left])
+destination = np.float32([
+                (bottom_left[0], 0),
+                (bottom_right[0], 0),
+                (bottom_right[0], HEIGHT - 1),
+                (bottom_left[0], HEIGHT - 1)
+                ])
+overhead_transform = cv2.getPerspectiveTransform(source, destination)
 
 capture = cv2.VideoCapture(0)
 
 while True:
+    # カメラ読み込み
     ret, frame = capture.read()
-    cv2.imshow(frame)
+    # print(frame.shape)
+    cv2.imshow("main1", frame)
+
+
+    # 範囲抽出
+    frame = cv2.warpPerspective(frame, overhead_transform, dsize=(WIDTH, HEIGHT))
+
+    cv2.imshow("main3", frame)
+
+    # 2値変換
+    img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    ret, frame = cv2.threshold(img_gray, 0, 255, cv2.THRESH_OTSU)
+    
+    # print(frame)
+    cv2.imshow("main2", frame)
+
+    # SUM
+    vec = np.sum(frame, axis=0)
+    vec = (vec - vec.min()) / (vec.max() - vec.min()) # normalize
+
+    indexL = np.arange(639, -1, -1)
+    indexR = np.arange(640)
+
+    vecL = vec[:640] * indexL
+    vecR = vec[640:] * indexR
+
+    vecL = np.sum(vecL)
+    vecR = np.sum(vecR)
+
+    pos = (vecR - vecL) / 50000
+
+    # print(pos)
+    if pos < 0.43:
+        powerR = 0
+        powerL = 150 + 15 * abs(pos - 0.145)
+        # print("left")
+    else:
+        # print("right")
+        powerL = 0
+        powerR = 150 + 15 * abs(pos - 0.145)
+
+    if powerR > 255:
+        powerR = 150
+    if powerL > 255:
+        powerL = 150
+
+    try:
+        motor_levels = f"{int(powerL)},{int(powerR)}\r\n" # 改行コードで終了を伝える
+        print(motor_levels)
+        ser.write(motor_levels.encode("shift-jis")) # 送信するときはバイト型にする
+    except:
+        motor_levels = "0,0\r\n" # 改行コードで終了を伝える
+        print(motor_levels)
+        ser.write(motor_levels.encode("shift-jis")) # 送信するときはバイト型にする
+
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
+motor_levels = f"0,0\r\n" # 改行コードで終了を伝える
+print(motor_levels)
+ser.write(motor_levels.encode("shift-jis")) # 送信するときはバイト型にする
 capture.release()
 cv2.destroyAllWindows()
