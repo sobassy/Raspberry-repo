a62b9d3782640ae4e6bbe2f8518486bf65469ea0
Sobassy <altoscience1110@gmail.com>
2021-08-12 10:38:09 +0900

    Update testapp.py

--- python/flask/testapp.py
+++ python/flask/testapp.py
@@ -6,7 +6,7 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12, GPIO.OUT)
 
-p = GPIO.PWM(12, 500)  # channel=12 frequency=500Hz
+p = GPIO.PWM(12, 50)  # channel=12 frequency=500Hz
 p.start(0)
 
 @app.route("/")
@@ -18,6 +18,9 @@ def led_pwm():
     led_power = request.args.get('power')
     if led_power:
         p.ChangeDutyCycle(int(led_power))
+        return f"set power: {led_power}"
+    else:
+        return "not changed."
 
 
 if __name__ == '__main__':
